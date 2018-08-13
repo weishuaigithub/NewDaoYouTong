24544756d1bdb42de0961d8e9791566655a609a7
魏帅 <834715680@qq.com>
2018-08-13 22:10:21 +0800

    测试 01

--- DaoYouTong/AppDelegate.m
+++ DaoYouTong/AppDelegate.m
@@ -26,6 +26,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //test 01
     //登录界面
     // 要使用百度地图，请先启动BaiduMapManager
     _mapManager = [[BMKMapManager alloc]init];
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+target 'DaoYouTong' do
+pod 'SDWebImage'
+pod 'Masonry'
+pod 'BaiduMapKit' #百度地图
+end
